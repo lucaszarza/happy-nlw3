0bb02441ec01493defc1882aa01ba381777e590d
Lucas Zarza <lucas-lsz@hotmail.com>
2020-10-16 00:01:02 -0300

    configurando back end

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,10 +1,11 @@
-import express from 'express';
+import express from "express";
+import "./database/connection";
+
+import routes from "./routes";
 
 const app = express();
 
-app.get('/users', (req,res)=>{
-  return res.json({message: 'Hello World'});
-})
+app.use(express.json());
+app.use(routes);
 
 app.listen(3333);
-
